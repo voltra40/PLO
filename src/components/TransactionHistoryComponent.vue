c225f47da60f6f84e81b78881f763efb81a08f50
voltra40 <voltra40@gmail.com>
2022-01-21 10:14:17 -0500

    designed front end for transactions page

--- src/components/TransactionHistoryComponent.vue
+++ src/components/TransactionHistoryComponent.vue
@@ -1,20 +1,34 @@
 <template>
-  <div id="transactionsContainer">
-    <h1 class="title">Transactions</h1>
-    <table class="table is-hoverable is-fullWidth is-striped">
-      <thead>
-        <tr>
-          <th v-for="column in columns" :key="column._id">
-            {{ column }}
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="row in data" :key="row._id">
-          <td class="has-text-centered" v-for="cell in row" :key="cell._id">{{ cell }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div id="transactions">
+    <h1 class="title has-text-centered">Transactions</h1>
+    <div class="columns">
+        <div class="column">
+            <table class="table is-hoverable is-fullWidth is-striped">
+            <thead>
+                <tr>
+                <th v-for="column in columns" :key="column._id">
+                    {{ column }}
+                </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="row in data" :key="row._id">
+                <td class="has-text-centered" v-for="cell in row" :key="cell._id">{{ cell }}</td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+        <div class="column is-narrow">
+            <div class="tags are-large has-addons">
+                <span class="tag is-dark"> Average Price </span>
+                <span class="tag">${{ averagePrice }} </span>
+            </div>
+            <div class="tags are-large has-addons">
+                <span class="tag is-dark"> Total </span>
+                <span class="tag">${{ total }} </span>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -25,6 +39,8 @@ export default {
     return {
       columns: [],
       data: [],
+      averagePrice: '',
+      total: '',
     };
   },
 
@@ -49,14 +65,23 @@ export default {
       }
     }
     this.data = data.slice(2);
+
+    let tempAverage = 0.0;
+    let total = 0.0;
+    for (let i = 0; i < this.data.length; i++) {
+        total += parseFloat(this.data[i][3]);
+        tempAverage += (parseFloat(this.data[i][2]).toFixed(2) * parseFloat(this.data[i][3]).toFixed(2));
+    }
+    this.averagePrice = (tempAverage / total).toFixed(2);
+    this.total = total;
   },
 };
 </script>
 
 <style>
-#transactionsContainer {
+#transactions {
   margin: auto;
   margin-top: 5%;
-  max-width: 70%;
+  max-width: 40%;
 }
 </style>
