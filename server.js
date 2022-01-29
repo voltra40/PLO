f9c519ebaabb102a500e7a09848f2f3b2c39adcb
voltra40 <voltra40@gmail.com>
2022-01-28 20:37:04 -0500

    handle routers

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use("/api/sleep-data", sleepRoute);
 // check if in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/dist'));
-  app.get('*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
   })
 }
